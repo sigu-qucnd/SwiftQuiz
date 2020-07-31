733e2644a2d7d565eadc5f4773e2a75d991cbe9c
sigu-qucnd <evt.sgtmt@gmail.com>
2020-08-01 04:21:10 +0900

    create swiftPrograms in develop #2

---
+++ SwiftQuiz/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftQuiz
+//
+//  Created by Ryo Nagasawa on 2020/08/01.
+//  Copyright © 2020 Ryo Nagasawa. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
