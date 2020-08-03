6209cc40916788782dd3932460c9dbaa86cc5b94
sigu-qucnd <evt.sgtmt@gmail.com>
2020-08-04 05:53:08 +0900

    create jsonfile and load json #3

--- SwiftQuiz/ContentView.swift
+++ SwiftQuiz/ContentView.swift
@@ -8,9 +8,27 @@
 
 import SwiftUI
 
+   struct User: Codable {
+       let name: String
+       let age: Int
+       let message: String?  // オプショナル型にする
+   }
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+       Button(action: {
+            // messageが省略された場合
+
+        
+        // ルートがArrayの場合
+        
+        guard let jsonData = try? getJSONData() else { return }
+        guard let users = try? JSONDecoder().decode([User].self, from: jsonData) else { return }
+         print("users: \(users[1].name)")
+
+        }){
+            Text("Button")
+        }
     }
 }
 
@@ -18,4 +36,11 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+func getJSONData() throws -> Data? {
+    guard let path = Bundle.main.path(forResource: "quiz", ofType: "json") else { return nil }
+    let url = URL(fileURLWithPath: path)
+
+    return try Data(contentsOf: url)
 }
